6a7436710b8868130b6ea29c5a2201b3d27a1896
Alex Rattray <rattray.alex@gmail.com>
2023-09-07 21:08:03 -0400

    initial commit for repro of https://github.com/openai/openai-node/issues/297

---
+++ pages/api/route.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { OpenAI as OpenAIClient } from "openai";
+
+type ResponseData = {
+  message: string;
+};
+
+export const runtime = "edge";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  const client = new OpenAIClient();
+  const result = await client.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: "hi",
+      },
+    ],
+    model: "gpt-3.5-turbo",
+  });
+  res.status(200).json({ message: JSON.stringify(result) });
+}
